4097dcbaca1c38993fa8fb4089c73b0d9e5949c4
Hovhannes <hovhannes.baghdasaryan.03@gmail.com>
2022-11-17 22:50:26 +0400

    add delete and detail department endpoint

--- djangoAPI/employee/urls.py
+++ djangoAPI/employee/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('department/', views.getAllDepartments),
-    path('department/create', views.createDepartment)
+    path('department/create', views.createDepartment),
+    path('department/<departmentId>', views.getDepartmentDetail),
+    path('department/<departmentId>/delete', views.departmentDelete),
 ]
--- djangoAPI/employee/views.py
+++ djangoAPI/employee/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
+
 from .models import Departments
 from .serializers import DepartmentSerializers
 
@@ -12,6 +14,18 @@ def getAllDepartments(request):
     return Response(department_serializer.data)
 
 
+@api_view(["GET"])
+def getDepartmentDetail(request, departmentId):
+    try:
+        departmentDetail = Departments.objects.get(pk=departmentId)
+        department_serializer = DepartmentSerializers(instance=departmentDetail)
+
+        return Response(department_serializer.data)
+    except Exception as err:
+
+        raise ValidationError(detail=err)
+
+
 @api_view(["POST"])
 def createDepartment(request):
     serializer = DepartmentSerializers(data=request.data)
@@ -21,4 +35,17 @@ def createDepartment(request):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST, template_name="Hovo")
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def departmentDelete(request, departmentId):
+    try:
+        departmentDetail = Departments.objects.get(pk=departmentId)
+        serialized_detail = DepartmentSerializers(departmentDetail)
+        departmentDetail.delete()
+
+        return Response(serialized_detail.data, status=status.HTTP_202_ACCEPTED)
+
+    except Exception as err:
+       raise ValidationError(detail=err)
